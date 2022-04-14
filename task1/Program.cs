45e9176c7e35b2fad8982fd2f9ea9705dce25081
Spoon2 <viktor.spoon2@gmail.com>
2022-04-15 02:26:38 +0400

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочит по 
+убыванию элементы каждой строки двумерного массива. */
+
+
+int[,] array = FillArray(4, 4);
+PrintArray(array);
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+            if (array[i, k] < array[i, k + 1])
+            {
+                int temp = 0;
+                temp = array[i, k];
+                array[i, k] = array[i, k + 1];
+                array[i, k + 1] = temp;
+            }
+        }
+    }
+
+PrintArray(array);
+Console.WriteLine();
+
+int[,] FillArray(int row, int column)
+{
+    int[,] array = new int[row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}"+" ");
+        }
+        Console.WriteLine();
+    }
+    
+}
